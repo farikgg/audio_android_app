0341f8c07a32ea4b5727187a9c731d7410a9b1e0
farikgg <farikfarik319@gmail.com>
2026-01-22 16:14:34 +0500

    Lifespan

--- src/app/main.py
+++ src/app/main.py
@@ -1,12 +1,33 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from src.app.config import get_settings
 from src.api.v1 import ai_endpoints
+from src.core.database import async_engine, get_async_session
+from src.core.logger import logger
 
-settings = get_settings()
 
-app = FastAPI(
-    title=settings.app_name
-)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger.info("Запуск приложения...")
+    logger.info("Приложение запущено!")
+    yield
 
-app.include_router(ai_endpoints.router, prefix="/api/v1")
+    logger.info("Остановка приложения...")
+    await async_engine.dispose()
+    logger.info("✅ Приложение остановлено")
+
+def create_application() -> FastAPI:
+    settings = get_settings()
+    application = FastAPI(
+        title=settings.app_name,
+        version="0.1.0",
+        lifespan=lifespan,
+        debug=settings.debug
+    )
+    application.include_router(ai_endpoints.router, prefix="/api/v1")
+
+    return application
+
+app = create_application()
